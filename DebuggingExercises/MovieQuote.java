4e242993aac71ba1636b409cb422a6fcc3565fe1
adambarnes1 <adam.barnes1@my.jcu.edu.au>
2016-08-02 09:01:17 +1000

    week 1 task 5 movie quote

---
+++ DebuggingExercises/MovieQuote.java
@@ -0,0 +1,9 @@
+/**
+  Adz on 2/08/2016.
+ */
+public class MovieQuote {
+    public static void main(String[] args)
+    {
+        System.out.println("Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of the darkness. For he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers.");
+    }
+}
