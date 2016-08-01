3a6a8aa568504fee07ede3fb550fd4449d4c3706
adambarnes1 <adam.barnes1@my.jcu.edu.au>
2016-08-02 09:32:04 +1000

    week 1 task 5 Chair table

---
+++ DebuggingExercises/TableandChairs.java
@@ -0,0 +1,13 @@
+/**
+ *  Adz on 2/08/2016.
+ */
+public class TableandChairs {
+    public static void main(String[] args)
+    {
+        System.out.println("x                   x");
+        System.out.println("x    xxxxxxxxxx     x");
+        System.out.println("x    x        x     X");
+        System.out.println("xxxx x        x xxxxx");
+        System.out.println("x  x x        x x   x");
+        System.out.println("x  x x        x x   x");
+}}
