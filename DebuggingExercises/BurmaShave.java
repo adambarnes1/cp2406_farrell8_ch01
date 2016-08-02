e277fb2a4e1ab708285d46583b7a9df18226adc4
adambarnes1 <adam.barnes1@my.jcu.edu.au>
2016-08-02 14:58:41 +1000

    week 1 task 13 four dialog boxes BurmaShave

---
+++ DebuggingExercises/BurmaShave.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class BurmaShave
+{
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Takes the \"H\" out of shave");
+        JOptionPane.showMessageDialog(null, "Saves complexion");
+        JOptionPane.showMessageDialog(null, "Saves time & money");
+        JOptionPane.showMessageDialog(null, "No brush no lather");
+    }}
