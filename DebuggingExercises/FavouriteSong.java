a2b21744f62b974a93047282e3202feca4ada249
adambarnes1 <adam.barnes1@my.jcu.edu.au>
2016-08-02 13:55:31 +1000

    week 1 task 5 Favorite Song 4 lines

---
+++ DebuggingExercises/FavouriteSong.java
@@ -0,0 +1,14 @@
+/**
+ *  Adz on 2/08/2016.
+ */
+
+public class FavouriteSong {
+    public static void main(String[] args)
+    {
+     System.out.println("I took a pill in Ibiza");
+     System.out.println("To show Avicii I was cool");
+     System.out.println("And when I finally got sober, felt 10 years older");
+     System.out.println("But fuck it, it was something to do");
+
+
+}}
