087f6b716205aafd6179d2efb4a4f320b84d51e4
adambarnes1 <adam.barnes1@my.jcu.edu.au>
2016-08-02 14:47:20 +1000

    week 1 task 5 Q11 edited comments file so that it is in dialog box from q10

---
+++ DebuggingExercises/CommentsDialogBox.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class CommentsDialogBox
+{
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Program comments are nonexcuting statements that you add to a file for the purpose of documentation \n line comment // \n Block comment /* ending with */ \n Javadoc comments /** ");
+
+    }}
