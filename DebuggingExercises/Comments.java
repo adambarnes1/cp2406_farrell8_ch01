aeb76956df6eef72441e978ac86327da4544db2e
adambarnes1 <adam.barnes1@my.jcu.edu.au>
2016-08-02 14:05:42 +1000

    week 1 task 5 comments file 3 diff types

---
+++ DebuggingExercises/Comments.java
@@ -0,0 +1,13 @@
+/**
+ *  on 2/08/2016.
+ */
+public class Comments {
+    public static void main(String[] args)
+    {
+        System.out.println("Program comments are nonexcuting statements that you add to a file for the purpose of documentation");
+        System.out.println("line comment // ");
+        System.out.println("Block comment /* ending with */");
+        System.out.println("Javadoc comments /** */");
+
+
+    }}
